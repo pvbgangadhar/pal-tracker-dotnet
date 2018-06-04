003b514097554d0435a6bba7ba31e6b2c4a9af60
pvbgangadhar <services.pentakota@gmail.com>
2018-06-04 16:01:09 -0600

    enter

--- src/PalTracker/Program.cs
+++ src/PalTracker/Program.cs
@@ -17,9 +17,17 @@ namespace PalTracker
             BuildWebHost(args).Run();
         }
 
+
+        // public static IWebHost BuildWebHost(string[] args) =>
+        //     WebHost.CreateDefaultBuilder(args)
+        //         .UseStartup<Startup>()
+        //         .Build();
+
         public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+       WebHostBuilder(args).Build();
+
+        public static IWebHostBuilder WebHostBuilder(string[] args) =>
+             WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
